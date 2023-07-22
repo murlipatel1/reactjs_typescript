5c843bab85ef136127b322d65435840f99e44896
Murli Patel <patelmurli15@gmail.com>
2023-07-22 22:46:17 +0530

    table added

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
-const Table = () => {
-  return (
-    <>
-    <div>Table</div>
-    </>
-  )
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
-export default Table
+const Table: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const columns: GridColDef[] = [
+    { field : 'userId' },
+    { field: 'id' },
+    { field: 'title',width:400 },
+    { field: 'body', width: 500}
+  ];
+
+  const rows: GridRowsProp = posts.map((post) => ({
+    userId: post.userId,
+    id: post.id,
+    title: post.title,
+    body: post.body,
+  }));
+
+  return (
+    <div>
+      <h1>Posts:</h1>
+      <div style={{ height: 500, width: '100%'}}>
+      <DataGrid  rows={rows} columns={columns}/>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
